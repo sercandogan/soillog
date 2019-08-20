ee9a70b790c3df1866466e6b40ff6194550e352b
Sercan Dogan <sercandogan@yandex.com>
2019-08-20 18:03:42 +0200

    Fix Type error of constants

--- src/constants.py
+++ src/constants.py
@@ -7,15 +7,15 @@ load_dotenv(find_dotenv())
 
 
 class Pins(Enum):
-    TEMP_HUMIDITY = os.getenv("PIN_TEMPERATURE_HUMIDITY")
+    TEMP_HUMIDITY = int(os.getenv("PIN_TEMPERATURE_HUMIDITY"))
 
     def __get__(self, instance, owner):
         return self.value
 
 
 class Channels(Enum):
-    MOISTURE_1 = os.getenv("CHANNEL_MOISTURE_1")
-    MOISTURE_2 = os.getenv("CHANNEL_MOISTURE_2")
+    MOISTURE_1 = int(os.getenv("CHANNEL_MOISTURE_1"))
+    MOISTURE_2 = int(os.getenv("CHANNEL_MOISTURE_2"))
 
     def __get__(self, instance, owner):
         return self.value
